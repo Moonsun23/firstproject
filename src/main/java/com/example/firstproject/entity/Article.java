539ea629d4e974d731ba81edc5863b131da1c013
Moonsun23 <socoooool90@gmail.com>
2024-01-23 10:47:19 +0900

    수정할 내용이 있는 경우에만 동작하도록 patch 메서드 추가

--- src/main/java/com/example/firstproject/entity/Article.java
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -20,8 +20,15 @@ public class Article {
     @Column
     private String content;
 
+    public void patch(Article article) {     // 수정할 내용이 있는 경우에만 동작하면 된다.
+        if (article.title != null)
+            this.title = article.title;
+        if (article.content != null)
+            this.content = article.content;
+    }
 
-    // Article 생성사 추가
+
+    // Article 생성자 추가
 
     // toString() 메서드 추가
 
