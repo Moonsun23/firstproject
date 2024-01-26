2860ddd6bd37925c08f5b12e7031dbc16d115729
Moonsun23 <socoooool90@gmail.com>
2024-01-26 11:09:56 +0900

    5장 셀프체크 - 회원조회 기능

--- src/main/java/com/example/firstproject/controller/MemberController.java
+++ src/main/java/com/example/firstproject/controller/MemberController.java
@@ -1,13 +1,19 @@
 package com.example.firstproject.controller;
 
 import com.example.firstproject.dto.MemberForm;
+import com.example.firstproject.entity.Article;
 import com.example.firstproject.entity.Member;
 import com.example.firstproject.repository.MemberRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Slf4j
@@ -20,7 +26,7 @@ public class MemberController {
         return "members/new";
     }
 
-    @PostMapping("/join/create")
+    @PostMapping("/join")
     public String createMember(MemberForm form){
         log.info(form.toString());
         // 1. DTO를 엔티티로 변환
@@ -30,6 +36,26 @@ public class MemberController {
         Member saved= memberRepository.save(member);
         log.info(saved.toString());
         return "";
+    }
+
+    @GetMapping("/members/{id}")
+    public String show(@PathVariable Long id, Model model){
+        // 1. id를 조회해 데이터 가져오기
+        Member memberEntity = memberRepository.findById(id).orElse(null);
+        // 2. 모델에 데이터 등록하기
+        model.addAttribute("member", memberEntity);
+        // 3. 뷰 페이지 반환하기
+        return "members/show";
+    }
+
+    @GetMapping("/members")
+    public String index(Model model){
+        // 1. 모든 데이터 가져오기
+        Iterable<Member> members = memberRepository.findAll();
+        // 2. 모델에 데이터 등록하기
+        model.addAttribute("members", members);
+        // 3. 뷰 페이지 설정하기
+        return "members/index";
     }
 
 }
